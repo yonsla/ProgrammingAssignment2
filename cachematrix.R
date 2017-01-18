41b50efac9428d7f4f5d09044aa6c0803b3bd403
Yehonatan <ysella@math.ucla.edu>
2017-01-17 21:05:32 -0800

    Filled in function details

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of the following two functions is to minimize computing time
+## when computing the inverse of a matrix.
+## This is good for situations when we repeatedly need calculations of the
+## inverse of a matrix, but the matrix might occassionally change.
+## The solution is, in case the inverse had already been computed, to keep track
+## of the answer by caching and thus avoid repeat calculation, while
+## recalculating the inverse only if the matrix had recently been changed.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a data structure, which we'll call a "cache matrix",
+## for keeping track and updating both a given matrix x and its inverse s.
+## More precisely, it returns a list of 4 functions, set and get
+## for setting and getting the matrix x and setInv and getInv
+## for setting and getting its inverse s.
+## In addition, we use the NULL value for s as a flag indicating the inverse
+## had not yet been calculated.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL;
+    set <- function(y) {
+        x <<- y;
+        s <<- NULL;
+    }
+    get <- function() {
+        x;
+    }
+    setInv <- function(myS) {
+        s <<- myS;
+    }
+    getInv <- function() {
+        s;
+    }
+    list(set=set, get=get, setInv=setInv, getInv=getInv);
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a "cache matrix" cacheX and returns the inverse of the
+## corresponding matrix. If this had already been computed,
+## it returns the computed value. Otherwise, it computes it.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheX, ...) {
+    ## Return a matrix that is the inverse of 'cacheX'
+    s <- cacheX$getInv();
+    if(is.null(s)) {
+        x <- cacheX$get();
+        s <- solve(x, ...);
+        cacheX$setInv(s);
+    }
+    s;
 }
